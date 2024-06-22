a260921581f458e04d8c5e6d4014a6616650bf83
nemopss <alexey_gladilin@mail.ru>
2024-06-22 03:24:57 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	valid := []string{"type", "echo", "exit"}
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
@@ -21,9 +24,19 @@ func main() {
 		args := strings.Split(cmd, " ")[1:]
 		switch mainCmd {
 		case "exit":
-			os.Exit(0)
+			code, err := strconv.Atoi(args[0])
+			if err != nil {
+				os.Exit(1)
+			}
+			os.Exit(code)
 		case "echo":
 			fmt.Fprint(os.Stdout, strings.Join(args, " ")+"\n")
+		case "type":
+			if slices.Contains(valid, args[0]) {
+				fmt.Fprint(os.Stdout, args[0]+" is a shell builtin\n")
+			} else {
+				fmt.Fprint(os.Stdout, args[0]+": not found\n")
+			}
 		default:
 			fmt.Fprint(os.Stdout, cmd+": command not found\n")
 		}
